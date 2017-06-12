8507933b4c4f7b747728bd86406b165b0cf7014c
TryIO <io.trydent@gmail.com>
2017-06-12 21:30:05 +0200

    added animated player

--- feedback-js/src/Feedback.vue
+++ feedback-js/src/Feedback.vue
@@ -23,9 +23,32 @@
       playground: null,
       loader: null,
       background: null,
+      entranceFront: null,
+      receptionist: null,
+      officialEntrance: null,
+      bradley: null,
+      tree: null,
       manifest: [{
-        src: 'test00.png',
+        src: 'background.png',
         id: 'background'
+      }, {
+        src: 'entrance_front.png',
+        id: 'entrance_front'
+      }, {
+        src: 'receptionist.png',
+        id: 'receptionist'
+      }, {
+        src: 'official_entrance.png',
+        id: 'officialEntrance'
+      }, {
+        src: 'conrad_walking.png',
+        id: 'conradWalking'
+      }, {
+        src: 'conrad_standing.png',
+        id: 'conradStanding'
+      }, {
+        src: 'tree.png',
+        id: 'tree'
       }]
     }),
     computed: {
@@ -33,6 +56,8 @@
       width () { return this.playground.canvas.width }
     },
     methods: {
+      asX (rectX) { return (rectX - 1) * 32 },
+      asY (rectY) { return (rectY - 1) * 32 },
       onComplete () {
         this.loading = false
         this.playground = new createjs.Stage('playground')
@@ -40,7 +65,61 @@
         this.background = new createjs.Shape()
         this.background.graphics.beginBitmapFill(this.loader.getResult('background')).drawRect(0, 0, this.width, this.height)
 
-        this.playground.addChild(this.background)
+        this.entranceFront = new createjs.Bitmap(this.loader.getResult('entrance_front'))
+        this.entranceFront.x = 288 - this.entranceFront.image.width
+        this.entranceFront.y = 32 * 27 - this.entranceFront.image.height
+
+        this.receptionist = new createjs.Bitmap(this.loader.getResult('receptionist'))
+        this.receptionist.x = 640 - this.receptionist.image.width
+        this.receptionist.y = 32 * 27 - this.receptionist.image.height
+
+        this.officialEntrance = new createjs.Bitmap(this.loader.getResult('officialEntrance'))
+        this.officialEntrance.x = 1024 - this.officialEntrance.image.width
+        this.officialEntrance.y = 32 * 27 - this.officialEntrance.image.height
+
+        this.tree = new createjs.Bitmap(this.loader.getResult('tree'))
+        this.tree.x = 974 - this.tree.image.width
+        this.tree.y = 32 * 27 - this.tree.image.height
+
+        let frames = [[0, 0, 28, 156, 0]]
+        for (let i = 0; i < 12; i++) frames.push([(i * 88), 0, 88, 156, 1])
+        console.table(frames)
+
+        const bradleySheet = new createjs.SpriteSheet({
+          framerate: 20,
+          images: [this.loader.getResult('conradStanding'), this.loader.getResult('conradWalking')],
+          frames: frames,
+          animations: {
+            standing: {frames: [0]},
+            walking: [1, 12, 'walking', 0.200]
+          }
+        })
+
+        this.bradley = new createjs.Sprite(bradleySheet, 'walking')
+        this.bradley.x = 64
+        this.bradley.y = 32 * 27 - 156
+
+        this.playground.addChild(
+          this.background,
+          this.receptionist,
+          this.tree,
+          this.bradley,
+          this.entranceFront,
+          this.officialEntrance
+        )
+
+        createjs.Ticker.timingMode = createjs.Ticker.RAF
+        createjs.Ticker.addEventListener('tick', this.tick)
+      },
+      tick (event) {
+        let deltaS = event.delta / 1000
+        let position = this.bradley.x + 122 * deltaS
+//        console.log(`Current position: ${position}`)
+//        let position = this.bradley.x + 2
+
+        let bradleyWidth = this.bradley.getBounds().width * this.bradley.scaleX
+        this.bradley.x = (position >= this.width + bradleyWidth) ? -bradleyWidth : position
+
         this.playground.update()
       }
     }
