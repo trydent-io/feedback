0274db3869bfc8ef8e5fee7f48fe56aace07fe5d
TryIO <io.trydent@gmail.com>
2017-06-26 01:06:59 +0200

    more stuff done on animations and movement altogether, looks good next step: some physics... well sort of (no Box2D involved)

--- feedback-js/src/doumun/index.js
+++ feedback-js/src/doumun/index.js
@@ -1,5 +1,5 @@
 import {Action, XAnimation, XSprite} from './doumun-sprite'
-import {keys, LEFT_ARROW, RIGHT_ARROW, SHIFT, UP_ARROW, ENTER} from '../engine/keyboard'
+import {keys, LEFT_ARROW, RIGHT_ARROW, SHIFT, UP_ARROW, ENTER, SPACEBAR, N1, N2} from '../engine/keyboard'
 
 export const BRADLEY_ID = 'conrad'
 
@@ -10,18 +10,19 @@ const JUMPING_FW = 'jumpingFW'
 
 const ANIMATIONS = {
   [STANDING]: new XAnimation(0),
-  [WALKING]: new XAnimation(1, 12, 0.400, WALKING),
-  [TURNING]: new XAnimation(13, 22, 0.400),
-  [JUMPING_FW]: new XAnimation(23, 42, 0.400, STANDING)
+  [WALKING]: new XAnimation(1, 12, 0.300, WALKING),
+  [TURNING]: new XAnimation(13, 22, 0.300, STANDING),
+  [JUMPING_FW]: new XAnimation(23, 42, 0.300, JUMPING_FW)
 }
 
-ANIMATIONS[JUMPING_FW].movement = 6
+ANIMATIONS[WALKING].movement = 32 / (11 * (1 / 0.300))
+ANIMATIONS[JUMPING_FW].movement = 16
 
 const FRAMES = {
   x: 0,
   y: 0,
   width: 320,
-  height: 156
+  height: 200
 }
 
 export function Bradley (image) {
@@ -35,18 +36,23 @@ export function Bradley (image) {
     isJumpingFw: {get: () => this.is(JUMPING_FW)},
 
     mustWalk: {get: () => ((keys[RIGHT_ARROW] && this.isRight) || (keys[LEFT_ARROW] && this.isLeft)) && this.isStanding},
+
+    doneMidStepWalk: {get: () => this.isWalking && this.atMidFrame},
+    doneLastStepWalk: {get: () => this.isWalking && this.frameIndex === 38},
+    isReversalPressed: {get: () => ((keys[LEFT_ARROW] && this.isRight) || (keys[RIGHT_ARROW] && this.isLeft))},
+    mustMidStand: {get: () => this.doneMidStepWalk && (this.isReversalPressed || keys.none)},
+    mustLastStand: {get: () => this.doneLastStepWalk && (this.isReversalPressed || keys.none)},
+
     doneWalkStep: {get: () => this.isWalking && (this.atMidFrame || this.atLastFrame)},
-    mustStand: {get: () => (keys.none && this.doneWalkStep)},
     mustJumpFw: {get: () => keys[UP_ARROW] && keys[SHIFT] && this.isStanding},
-    mustTurn: {get: () => ((keys[RIGHT_ARROW] && this.isLeft) || (keys[LEFT_ARROW] && this.isRight)) && (this.isStanding || this.doneWalkStep)},
+    mustTurn: {get: () => this.isReversalPressed && this.isStanding},
     endedTurn: {get: () => this.isTurning && this.frameIndex === 23}
   })
 
   this.stand = () => this.play(STANDING)
   this.walk = () => this.play(WALKING)
-  this.turn = () => {
-    this.play(TURNING)
-  }
+  this.turn = () => this.play(TURNING)
+  this.jumpFw = () => this.play(JUMPING_FW)
 
   this.ticked = playground => {
 /*
@@ -57,19 +63,31 @@ export function Bradley (image) {
       Len: ${this.animation.length}
       `))
 */
-    Action(this.isWalking && this.changedTimedFrame, this.move)
 
     Action(this.mustWalk, this.walk)
-    Action(this.mustStand, this.stand)
+    Action(this.mustMidStand, () => {
+      this.stand()
+      this.x += (64 * this.scaleX)
+    })
+    Action(this.doneLastStepWalk, () => {
+      this.x += (128 * this.scaleX)
+      Action(keys.none, this.stand)
+    })
     Action(this.mustTurn, this.turn)
 
-    Action(this.mustJumpFw, () => this.play(JUMPING_FW))
-    Action(this.isJumpingFw && this.betweenFrames(7, 12), this.move)
+    Action(this.mustJumpFw, this.jumpFw)
+    Action(this.isJumpingFw && this.betweenFrames(7, 12), () => { this.x += (120 / (6 * (1 / this.animations[JUMPING_FW].mills))) * this.scaleX })
     Action(this.isJumpingFw && this.betweenFrames(7, 9), () => { this.y -= 4 })
     Action(this.isJumpingFw && this.betweenFrames(9, 11), () => { this.y += 4 })
-    Action(this.isJumpingFw && this.atLastFrame, () => { this.x += (4 * 32) * this.scaleX })
+    Action(this.isJumpingFw && this.frameIndex === 64, () => {
+      this.stand()
+      this.x += (128 + 8) * this.scaleX
+    })
 
     Action(keys[ENTER], () => console.log(`Position: ${this.x}`))
+    Action(keys[SPACEBAR], () => { this.x = 32 })
+    Action(keys[N1], () => console.log(`Walking length: ${ANIMATIONS[WALKING].length}`))
+    Action(keys[N2], () => console.log(`Jumping Fw length: ${ANIMATIONS[JUMPING_FW].length}`))
 
     Action(this.endedTurn, () => {
       this.flip()
--- feedback-js/src/stage/entrance-stage.js
+++ feedback-js/src/stage/entrance-stage.js
@@ -4,6 +4,7 @@ import {Bradley, BRADLEY_ID} from '../doumun'
 const manifest = (id) => ({id: id, src: `${id}.png`})
 
 const BACKGROUND = manifest('background')
+const LEVEL1 = manifest('level1')
 const ENTRANCE_FRONT = manifest('entrance_front')
 const RECEPTIONIST = manifest('receptionist')
 const OFFICIAL_ENTRANCE = manifest('official_entrance')
@@ -15,6 +16,7 @@ export default {
     console.log('Before the load!')
     assets.manifest = [
       BACKGROUND,
+      LEVEL1,
       TREE,
       RECEPTIONIST,
       BRADLEY,
@@ -24,8 +26,9 @@ export default {
   },
   loaded (assets, stage) {
     this.background = new createjs.Shape()
-    this.background.graphics.beginBitmapFill(assets.get(BACKGROUND.id)).drawRect(0, 0, stage.canvas.width, stage.canvas.height)
-
+    // this.background.graphics.beginBitmapFill(assets.get(BACKGROUND.id)).drawRect(0, 0, stage.canvas.width, stage.canvas.height)
+    this.background.graphics.beginBitmapFill(assets.get(LEVEL1.id)).drawRect(0, 0, stage.canvas.width, stage.canvas.height)
+/*
     this.entranceFront = new createjs.Bitmap(assets.get(ENTRANCE_FRONT.id))
     this.entranceFront.x = 288 - this.entranceFront.image.width
     this.entranceFront.y = 32 * 27 - this.entranceFront.image.height
@@ -41,18 +44,18 @@ export default {
     this.tree = new createjs.Bitmap(assets.get(TREE.id))
     this.tree.x = 974 - this.tree.image.width
     this.tree.y = 32 * 27 - this.tree.image.height
-
+*/
     this.bradley = new Bradley(assets.get(BRADLEY.id))
     this.bradley.x = 32
-    this.bradley.y = 32 * 27
+    this.bradley.y = 32 * 9
 
     stage.addChild(
       this.background,
-      this.tree,
-      this.receptionist,
+      // this.tree,
+      // this.receptionist,
       this.bradley,
-      this.entranceFront,
-      this.officialEntrance
+      // this.entranceFront,
+      // this.officialEntrance
     )
   },
 
